729e67ae46ac576b87324096b54f9eb78697186f
David Fisher <fisherds@rose-hulman.edu>
2020-04-15 10:59:51 -0400

    proof that VS Code can program

--- TrafficLightArduinoLibrary/TrafficLightAruinoLibrary.ino
+++ TrafficLightArduinoLibrary/TrafficLightAruinoLibrary.ino
@@ -1,4 +1,4 @@
-#define PIN_LED_RED			10
+#define PIN_LED_RED 10
 #define PIN_LED_YELLOW		9
 #define PIN_LED_GREEN		6
 #define PIN_LED_BLUE		5
@@ -20,13 +20,12 @@ void setup() {
 
 void loop() {
   if (digitalRead(PIN_PUSHBUTTON_BLUE) == PRESSED) {
-    digitalWrite(PIN_LED_RED, HIGH);
+    // digitalWrite(PIN_LED_RED, HIGH);
     digitalWrite(PIN_LED_YELLOW, HIGH);
     digitalWrite(PIN_LED_GREEN, HIGH);
-    Serial.println("Pressing button");
-
+    Serial.println("Pressing button via VS Code");
   } else {
-    digitalWrite(PIN_LED_RED, LOW);
+    // digitalWrite(PIN_LED_RED, LOW);
     digitalWrite(PIN_LED_YELLOW, LOW);
     digitalWrite(PIN_LED_GREEN, LOW);
   }
