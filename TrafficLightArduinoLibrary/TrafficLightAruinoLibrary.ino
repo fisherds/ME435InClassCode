b385a4178c5159b456641bde43e0d91cf3cdb37e
David Fisher <fisherds@rose-hulman.edu>
2020-04-15 10:52:19 -0400

    initialize my arduino sketch

--- TrafficLightArduinoLibrary/TrafficLightAruinoLibrary.ino
+++ TrafficLightArduinoLibrary/TrafficLightAruinoLibrary.ino
@@ -1,8 +1,34 @@
-void setup () {
-  // Hello github
+#define PIN_LED_RED			10
+#define PIN_LED_YELLOW		9
+#define PIN_LED_GREEN		6
+#define PIN_LED_BLUE		5
+
+#define PRESSED 0
+#define UNPRESSED 1
+
+#define PIN_PUSHBUTTON_BLUE	0
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_YELLOW, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
+  
+  pinMode(PIN_PUSHBUTTON_BLUE, INPUT_PULLUP);  
 }
 
 void loop() {
+  if (digitalRead(PIN_PUSHBUTTON_BLUE) == PRESSED) {
+    digitalWrite(PIN_LED_RED, HIGH);
+    digitalWrite(PIN_LED_YELLOW, HIGH);
+    digitalWrite(PIN_LED_GREEN, HIGH);
+    Serial.println("Pressing button");
 
+  } else {
+    digitalWrite(PIN_LED_RED, LOW);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+    digitalWrite(PIN_LED_GREEN, LOW);
+  }
+  delay(100); // Optional
 }
-
