6f95d034792ef471fa3daed4432fa9b00f0dcf4d
David Fisher <fisherds@rose-hulman.edu>
2020-05-12 12:15:48 -0400

    Led On Off board code

---
+++ 04_Communication/LedOnOff/LedOnOff.ino
@@ -0,0 +1,34 @@
+String inputString = "";      // a String to hold incoming data
+volatile bool stringComplete = false;  // whether the string is complete
+
+void setup() {
+  Serial.begin(9600);
+  inputString.reserve(200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (stringComplete) {
+    // Do stuff with the message.
+    if (inputString.equalsIgnoreCase("ON")) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else if (inputString.equalsIgnoreCase("OFF")) {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    // Prepare for the next message
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    } else {
+      inputString += inChar;
+    }
+  }
+}
